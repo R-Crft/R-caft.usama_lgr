b6b7ef587a55a75a7c62ff4d4fbec497584fd385
R-Crft <163010332+R-Crft@users.noreply.github.com>
2024-03-11 14:23:30 +0000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+function joinDiscordServer() {
+  // Replace 'YOUR_DISCORD_SERVER_INVITE_LINK' with your actual Discord server invite link
+  var discordInviteLink = "https://discord.gg/aEF2wQ3p";
+
+  // Open the invite link in a new tab
+  window.open(discordInviteLink, "_blank");
+}
